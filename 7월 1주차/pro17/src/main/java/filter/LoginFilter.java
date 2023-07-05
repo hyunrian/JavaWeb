4a49d615a2d3b9ea95259a274cb4463e2c315af1
HyunRian <121999215+hyunrian@users.noreply.github.com>
2023-07-05 14:27:12 +0900

    게시판 - 댓글 입력/조회

--- 7월 1주차/pro17/src/main/java/filter/LoginFilter.java
+++ 7월 1주차/pro17/src/main/java/filter/LoginFilter.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebFilter("/board/*")
+@WebFilter({"/board/*", "/comment/*"})
 public class LoginFilter extends HttpFilter implements Filter {
        
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
